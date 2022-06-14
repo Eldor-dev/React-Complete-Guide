06edf1e4bda01542423d486f3e28dc343f0de846
Eldor Azimov <eldorazimov37@gmail.com>
2022-06-14 22:11:54 +0900

    Rendering List of Data

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -18,7 +18,16 @@ const Expenses = ({ items }) => {
           selected={filteredYear}
           onChangeFiltered={filteredChangeHandler}
         />
-        <ExpenseItem
+        {items.map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))}
+
+        {/* <ExpenseItem
           title={items[0].title}
           amount={items[0].amount}
           date={items[0].date}
@@ -37,7 +46,7 @@ const Expenses = ({ items }) => {
           title={items[3].title}
           amount={items[3].amount}
           date={items[3].date}
-        />
+        /> */}
       </Card>
     </div>
   );
