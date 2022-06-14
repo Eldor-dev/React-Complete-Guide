ba4313425946c814a746801e59f4ed725805e1af
Eldor Azimov <eldorazimov37@gmail.com>
2022-06-14 19:26:46 +0900

    Child-to-Parent Component Comunication

--- src/App.js
+++ src/App.js
@@ -24,9 +24,14 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <NewExpenses />
+      <NewExpenses onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
